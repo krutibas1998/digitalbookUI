0f99efb3e2aad9eb42fc78021c91c2935e4c10ad
krutibas1998 <krutibaslenka1998@gmail.com>
2022-08-29 01:27:41 +0530

    latestcommit

--- src/app/createbook/createbook.component.ts
+++ src/app/createbook/createbook.component.ts
@@ -25,6 +25,7 @@ export class CreatebookComponent implements OnInit {
     createdDate:'',
     modifiedDate:''
   }
+
   ngOnInit(): void {
     
   }
@@ -56,6 +57,7 @@ export class CreatebookComponent implements OnInit {
            createdDate:'',
            modifiedDate:''
           };
+        
         }
       );
     }
